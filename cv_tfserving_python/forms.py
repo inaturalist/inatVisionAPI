07a5a2935b7ad5eb16c6ac3968411f8043656d40
Patrick Leary <patrick.r.leary@gmail.com>
2017-04-21 17:40:22 -0400

    going with the tf-session-reuse approach and removing the others

--- cv_tfserving_python/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import Form
-from flask_wtf.file import FileField, FileRequired
-
-class ImageForm(Form):
-    image = FileField('image',
-        validators=[
-            FileRequired(message="Please include 'image' field.")
-        ])
