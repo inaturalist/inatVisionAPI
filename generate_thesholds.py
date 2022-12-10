2819ad1007ed7104c6c12c495fa1d593dce4edf9
loarie <loarie@gmail.com>
2022-12-09 22:58:28 -0800

    create pseudoabsences from training data

--- generate_thesholds.py
+++ generate_thesholds.py
@@ -1,10 +1,11 @@
 """
-Script to generate thresholds from a tensor flow model and test data
+Script to generate thresholds from a tensor flow model, taxonomu, test and train data
 """
 
 import argparse
 from tqdm.auto import tqdm
 import pandas as pd
+import h3pandas
 import numpy as np
 import math
 from sklearn.metrics import precision_recall_curve
@@ -13,6 +14,60 @@ from sklearn.metrics import auc
 from lib.taxon import Taxon
 from lib.model_taxonomy import ModelTaxonomy
 from lib.tf_gp_model import TFGeoPriorModel
+
+def ratios_for_taxon_id(taxon_id, all_spatial_grid_counts, train_df_h3):
+    
+    #counts in each presence cell for target taxon
+    target_spatial_grid_counts = train_df_h3[train_df_h3.taxon_id==taxon_id].index.value_counts()
+
+    #background count per target taxon count in target taxon presence cells
+    counts = pd.DataFrame({
+        "target": target_spatial_grid_counts,
+        "all": all_spatial_grid_counts,
+    })
+    counts = counts.fillna(0)
+    counts["target_ratio"] = counts["all"]/counts["target"]
+    counts.replace([np.inf, -np.inf], np.nan, inplace=True)
+    good_counts = counts["target_ratio"].dropna()
+    return good_counts
+
+def pseudo_absences_for_taxon_id(
+        taxon_id,
+        all_spatial_grid_counts,
+        train_df_h3,
+        test_df_h3,
+        full_spatial_data_lookup_table
+    ):
+    
+    #count cutoff is mean background count per target taxon count in target taxon presence cells
+    count_cutoff = ratios_for_taxon_id(taxon_id, all_spatial_grid_counts, train_df_h3).mean()
+    
+    #absence candidates from test
+    sample = test_df_h3.sample(10_000)
+    sample_occupancy_pseudoabsences = []
+    for i, row in sample.iterrows():
+        if taxon_id in full_spatial_data_lookup_table.loc[row.name].taxon_id:
+            #candidate in cell containing target taxon so can't be absence
+            pass
+        else:
+            if row.name in all_spatial_grid_counts.index:
+                if all_spatial_grid_counts.loc[row.name] > count_cutoff:
+                    #candidate in cell not containing target taxon and
+                    #count is greater than count cutoff. Use as absence
+                    sample_occupancy_pseudoabsences.append(
+                        (row.lat, row.lng)
+                    )
+                
+        #limit number of absences
+        if len(sample_occupancy_pseudoabsences) >= 500:
+            break
+
+    sample_occupancy_pseudoabsences = pd.DataFrame(
+        sample_occupancy_pseudoabsences,
+        columns=["lat", "lng"]
+    )
+    sample_occupancy_pseudoabsences["taxon_id"] = taxon_id
+    return sample_occupancy_pseudoabsences
 
 def get_threshold(test, predictions):
   
@@ -38,20 +93,44 @@ def get_predictions(latitude,longitude,taxon_id, mt, tfgpm):
 
 def main(args):
     
+    print("loading in test and train data...")
+    test_df = pd.read_csv(args.test_spatial_data_path,
+        usecols=["taxon_id","latitude","longitude"]).rename({
+        "latitude": "lat",
+        "longitude": "lng"
+    }, axis=1)    
+    train_df = pd.read_csv(args.train_spatial_data_path,
+        usecols=["taxon_id","latitude","longitude"]).rename({
+        "latitude": "lat",
+        "longitude": "lng"
+    }, axis=1)
+    taxon_ids = test_df.taxon_id.unique()
+    
+    print("calculating absences...")
+    h3_resolution = 3
+    test_df_h3 = train_df.h3.geo_to_h3(h3_resolution)
+    train_df_h3 = train_df.h3.geo_to_h3(h3_resolution)
+    full_spatial_data_lookup_table = pd.concat([train_df_h3, test_df_h3]).pivot_table(
+        index="h3_0"+str(h3_resolution),
+        values="taxon_id",
+        aggfunc=set
+    )
+    pseudoabsence_df = pd.DataFrame()
+    all_spatial_grid_counts = train_df_h3.index.value_counts()
+    for taxon_id in tqdm(taxon_ids):
+        pa = pseudo_absences_for_taxon_id(
+            taxon_id,
+            all_spatial_grid_counts,
+            train_df_h3,
+            test_df_h3,
+            full_spatial_data_lookup_table
+        )
+        pseudoabsence_df = pd.concat([pseudoabsence_df, pa])
+    
+    print("calculating thresholds...")
     mt = ModelTaxonomy(args.taxonomy_path)
     tfgpm = TFGeoPriorModel(args.model_path, mt)
-    test_df = pd.read_csv(args.test_spatial_data_path,
-        usecols=["taxon_id","latitude","longitude"])
-    pseudoabsence_df = pd.read_csv(args.absences_path,
-        usecols=["lat","lng","occupancy_pseudo_absence_taxonid"]).rename({
-            "lat": "latitude",
-            "lng": "longitude",
-            "occupancy_pseudo_absence_taxonid": "taxon_id"
-        }, axis=1)
-    
-    #get the set of taxa
-    taxon_ids = pseudoabsence_df.taxon_id.unique()
-    
+        
     output = []
     for taxon_id in tqdm(taxon_ids):
         absences = pseudoabsence_df[pseudoabsence_df.taxon_id==taxon_id]
@@ -59,13 +138,13 @@ def main(args):
         test = np.concatenate(([1] * len(presences), [0] * len(absences)))
         
         longitude = np.concatenate((
-            presences.longitude.values.astype('float32'),
-            absences.longitude.values.astype('float32')
+            presences.lng.values.astype('float32'),
+            absences.lng.values.astype('float32')
         ))
         
         latitude = np.concatenate((
-            presences.latitude.values.astype('float32'),
-            absences.latitude.values.astype('float32')
+            presences.lat.values.astype('float32'),
+            absences.lat.values.astype('float32')
         ))
         
         predictions = get_predictions(latitude, longitude, taxon_id, mt, tfgpm)
@@ -81,8 +160,8 @@ def main(args):
         row_dict = dict(row)
         output.append(row_dict)
         
-        #write results to file
-        pd.DataFrame(output).to_csv(args.output_path)
+    print("writing output...")
+    pd.DataFrame(output).to_csv(args.output_path)
 
 if __name__ == "__main__":
     
@@ -90,9 +169,8 @@ if __name__ == "__main__":
                '   python generate_thresholds.py --model_path tf_geoprior_1_4_r6.h5 \n' + \
                '   --taxonomy_path taxonomy_1_4.csv\n' + \
                '   --test_spatial_data_path test_spatial_data.csv\n' + \
-               '   --absences_path occupancy_pseudo_absences.csv\n' + \
+               '   --train_spatial_data_path test_spatial_data.csv\n' + \
                '   --output_path thresholds.csv\n'
-    
     
     parser = argparse.ArgumentParser(usage=info_str)
     parser.add_argument('--model_path', type=str,
@@ -100,9 +178,9 @@ if __name__ == "__main__":
     parser.add_argument('--taxonomy_path', type=str,
                         help='Path to taxonomy csv.', required=True)
     parser.add_argument('--test_spatial_data_path', type=str,
-                        help='Path to test (presence) csv.', required=True)
-    parser.add_argument('--absences_path', type=str,
-                        help='Path to absence csv.', required=True)
+                        help='Path to test csv for presence/absences.', required=True)
+    parser.add_argument('--train_spatial_data_path', type=str,
+                        help='Path to train csv for occupancy.', required=True)
     parser.add_argument('--output_path', type=str,
                         help='Path to write thesholds.', required=True)
     args = parser.parse_args()
