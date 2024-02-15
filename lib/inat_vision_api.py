a5397dcb8c7a1d59d6e7640897c51d6fbf1714aa
sylvain-morin <sylvain.morin@inaturalist.org>
2024-02-15 12:40:47 +0100

    Test

--- lib/inat_vision_api.py
+++ lib/inat_vision_api.py
@@ -242,7 +242,7 @@ class InatVisionAPI:
         # fetch observation details using the API
         response = urllib.request.urlopen(url)
         data = json.loads(response.read())
-        if (data is None or data["results"] is None or data["results"][0] is
+        if (data is None or data["results"] is None or len ( data["results"] ) = 0 or data["results"][0] is
             None or data["results"][0]["photos"] is None or data["results"][0]["photos"][0] is
                 None or data["results"][0]["photos"][0]["url"] is None):
             return None, None, None
