5391732f728a9d353297b126f8f01403f7e565a3
Alex Shepard <alex@meatfreezer.com>
2025-03-06 13:54:12 -0800

    whitespace & formatting

--- tests/test_vision_inferrer.py
+++ tests/test_vision_inferrer.py
@@ -10,7 +10,10 @@ class TestVisionInferrer:
         model_path = "model_path"
         inferrer = VisionInferrerTF(model_path)
         assert inferrer.model_path == model_path
-        tf.keras.models.load_model.assert_called_once_with(model_path, compile=False)
+        tf.keras.models.load_model.assert_called_once_with(
+            model_path,
+            compile=False
+        )
 
     def test_process_image(self, mocker):
         mocker.patch("tensorflow.keras.models.load_model", return_value=MagicMock())
@@ -20,5 +23,6 @@ class TestVisionInferrer:
         theimage = "theimage"
         inferrer.process_image(theimage)
         inferrer.layered_model.assert_called_once_with(
-            tf.convert_to_tensor(theimage), training=False
+            tf.convert_to_tensor(theimage),
+            training=False
         )
