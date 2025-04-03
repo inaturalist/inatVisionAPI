8a84fe982e9b05775d998012e877617b2f1b185c
Alex Shepard <alex@meatfreezer.com>
2025-04-03 08:52:27 -0700

    thresholding speedups
    first, use parquet files. faster than csvs in pandas.
    
    second, set taxon id index on h3 df
    we query on this repeatedly. produces 20x speedups.

--- generate_thresholds.py
+++ generate_thresholds.py
@@ -21,6 +21,42 @@ def ignore_shapely_deprecation_warning(message, category, filename, lineno, file
         return None
     return warnings.defaultaction(message, category, filename, lineno, file, line)
 
+def _load_train_data_csv(path):
+    print("loading in the training data from csv...")
+    train_df = pd.read_csv(
+        path,
+        usecols=[
+            "taxon_id",
+            "latitude",
+            "longitude",
+            "captive"
+        ]
+    )
+    return train_df
+
+def _load_train_data_parquet(path):
+    print("loading in the training data from parquet...")
+    train_df = pd.read_parquet(path)
+    train_df = train_df[["taxon_id", "latitude", "longitude", "captive"]]
+    return train_df
+
+def _load_train_data(path):
+    if path.endswith(".csv"):
+        train_df = _load_train_data_csv(path)
+    elif path.endswith(".parquet"):
+        train_df = _load_train_data_parquet(path)
+    else:
+        assert False, "spatial data train df format not supported."
+    
+    train_df.rename({
+        "latitude": "lat",
+        "longitude": "lng",
+    }, axis=1, inplace=True)
+    train_df = train_df[train_df.captive==0] # no-CID ok, wild only
+    train_df.drop(["captive"], axis=1)
+    return train_df
+ 
+
 
 def main(args):
     print("loading in the model...")
@@ -56,28 +92,16 @@ def main(args):
     )
 
     print("loading in the training data...")
-    train_df = pd.read_csv(
-        args.train_spatial_data,
-        usecols=[
-            "taxon_id",
-            "latitude",
-            "longitude",
-            "captive"
-        ]
-    ).rename({
-        "latitude": "lat",
-        "longitude": "lng"
-    }, axis=1)
-    train_df = train_df[train_df.captive == 0]  # no-CID ok, wild only
-    train_df.drop(["captive"], axis=1)
+    train_df = _load_train_data(args.train_spatial_data)
     train_df_h3 = train_df.h3.geo_to_h3(args.h3_resolution)
+
     all_spatial_grid_counts = train_df_h3.index.value_counts()
     presence_absence = pd.DataFrame({
         "background": all_spatial_grid_counts,
     })
     presence_absence = presence_absence.fillna(0)
 
-    print("...looping through taxa")
+    print("loading taxonomy...")
     output = []
     taxa = pd.read_csv(
         args.taxonomy,
@@ -92,6 +116,12 @@ def main(args):
         taxon_ids = taxon_ids[0:args.stop_after]
     resolution = args.h3_resolution
     area = h3.hex_area(resolution)
+
+    # we want the taxon id to be the index since we'll be selecting on it
+    train_df_h3.reset_index(inplace=True)
+    train_df_h3.set_index("taxon_id", inplace=True)    
+    
+    print("...looping through taxa")
     for taxon_id in tqdm(taxon_ids):
         try:
             class_of_interest = mtd.df.loc[taxon_id]["leaf_class_id"]
@@ -105,7 +135,7 @@ def main(args):
 
         # make presence absence dataset
         target_spatial_grid_counts = \
-            train_df_h3[train_df_h3.taxon_id == taxon_id].index.value_counts()
+            train_df_h3[train_df_h3.index == taxon_id].h3_04.value_counts()
         presences = gdfk.loc[target_spatial_grid_counts.index]["pred"]
         if len(presences) == 0:
             print("not present")
